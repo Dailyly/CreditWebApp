766dc6cb57389f45a135eaae3c9d07a29ba8602c
Dailyly <daily1738451756@163.com>
2018-10-22 18:03:56 +0800

    save

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,8 +2,19 @@ import store from '../store'
 
 const {url} = store.getters.apiConfig
 
-export function codeErrorHandler (response, curtomHandler) {
+const notifyQueue = []
+export function notifyMsg (msg) {
+  if (notifyQueue.includes(msg)) return
+  notifyQueue.push(msg)
   
+}
+
+export function codeErrorHandler (response, curtomHandler) {
+  const { retCode, retMsg } = response
+  let msg = retMsg || '请求失败'
+  if (retCode !== 'SUCCESS') {
+    curtomHandler ? curtomHandler(retCode, notifyMsg) : notifyMsg(msg)
+  }
 }
 
 export default{
@@ -11,5 +22,5 @@ export default{
     baseURL: url
   },
   resCheck: (res) => (res.retCode === 'SUCCESS'),
-  
+  forceAxios: true,
 }
